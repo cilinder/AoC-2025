2973e9ca7583bc78205ec53f86a6fcc0e64e37df
cilinder <jure.taslak@gmail.com>
2025-12-09 19:08:13 +0100

    Day5 🎉

--- day5.go
+++ day5.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 )
 
@@ -45,6 +46,38 @@ func countFresh(ingredients []int, freshRanges [][]int) int {
 	return total
 }
 
+func mergeRanges(freshRanges [][]int) [][]int {
+	slices.SortFunc(freshRanges, func(r1, r2 []int) int {
+		if r1[0] == r2[0] {
+			return r1[1] - r2[1]
+		} else {
+			return r1[0] - r2[0]
+		}
+	})
+
+	merged := make([][]int, 0)
+	currRange := freshRanges[0]
+	for i := 1; i < len(freshRanges); i++ {
+		r := freshRanges[i]
+		if r[0] <= currRange[1] {
+			currRange[1] = max(currRange[1], r[1])
+		} else {
+			merged = append(merged, currRange)
+			currRange = r
+		}
+	}
+	merged = append(merged, currRange)
+	return merged
+}
+
+func countPotentialFresh(freshRanges [][]int) int {
+	total := 0
+	for _, r := range freshRanges {
+		total += r[1] - r[0] + 1
+	}
+	return total
+}
+
 func day5() {
 	filePath := filepath.Join("inputs", "day5.in")
 	data, err := os.ReadFile(filePath)
@@ -53,4 +86,6 @@ func day5() {
 	freshRanges, ingredients := splitData(lines)
 	numFresh := countFresh(ingredients, freshRanges)
 	fmt.Printf("There are %d fresh ingredients\n", numFresh)
+	merged := mergeRanges(freshRanges)
+	fmt.Printf("Total potential fresh ingredients: %d\n", countPotentialFresh(merged))
 }
