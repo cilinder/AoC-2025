579bad8757fd2afd48fb0694c50f2ea82876ff28
cilinder <jure.taslak@gmail.com>
2025-12-13 14:16:12 +0100

    Day7 part2 🎉

--- day7.go
+++ day7.go
@@ -13,6 +13,12 @@ func printManifold(manifold []string) {
 	}
 }
 
+func printCounts(counts [][]int) {
+	for _, row := range counts {
+		fmt.Println(row)
+	}
+}
+
 func replaceAtIndex(str string, replacement rune, index int) string {
     return str[:index] + string(replacement) + str[index+1:]
 }
@@ -36,12 +42,50 @@ func simulateBeam(tachyonManifold []string) int {
 	return splits
 }
 
+func manyWorlds(tachyonManifold []string) [][]int {
+	tachyonManifold[0] = strings.Replace(tachyonManifold[0], "S", "|", 1)
+	counts := make([][]int, len(tachyonManifold))
+	for i := 0; i < len(tachyonManifold); i++ {
+		counts[i] = make([]int, len(tachyonManifold[0]))
+	}
+	for i := 0; i < len(counts[0]); i++ {
+		if tachyonManifold[0][i] == '|' {
+			counts[0][i] = 1
+		}
+	}
+	for i := 1; i < len(tachyonManifold); i++ {
+		prev := tachyonManifold[i-1]
+		curr := tachyonManifold[i]
+		for j := 0; j < len(curr); j++ {
+			count := 0
+			if curr[j] == '|' {
+				if prev[j] == '|' {
+					count += counts[i-1][j]
+				}
+				if j > 0 && curr[j-1] == '^' {
+					count += counts[i-1][j-1]
+				}
+				if j < len(curr)-1 && curr[j+1] == '^' {
+					count += counts[i-1][j+1]
+				}
+			}
+			counts[i][j] = count
+		}
+	}
+	return counts
+}
+
 func day7() {
 	fileName := filepath.Join("inputs", "day7.in")
 	contents, err := os.ReadFile(fileName)
 	check(err)
 	rows := strings.Split(string(contents), "\n")
 	splits := simulateBeam(rows)
-	// printManifold(rows)
 	fmt.Printf("Total splits: %d\n", splits)
+	counts := manyWorlds(rows)
+	total := 0
+	for i := 0; i < len(counts[0]); i++ {
+		total += counts[len(counts)-1][i]
+	}
+	fmt.Println(total)
 }
