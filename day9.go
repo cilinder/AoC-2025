2233076cc692614ec4532e2aa2b6e4fcbcc3bbaf
Ajda Lampe <ajda.lampe@gmail.com>
2025-12-22 00:01:41 +0100

    Day 9 - Part 2 WIP

--- day9.go
+++ day9.go
@@ -12,6 +12,19 @@ type RedTile struct {
 	y int
 }
 
+type Directions struct {
+	north bool
+	south bool
+	east  bool
+	west  bool
+}
+
+type DirectionGrid struct {
+	offsetX int
+	offsetY int
+	flags   [][]Directions
+}
+
 func parseTiles(rows []string) []RedTile {
 	tiles := make([]RedTile, 0)
 	for _, row := range rows {
@@ -19,16 +32,16 @@ func parseTiles(rows []string) []RedTile {
 		tiles = append(tiles, RedTile{toInt(parts[0]), toInt(parts[1])})
 	}
 	return tiles
-} 
+}
 
 func area(tile1, tile2 RedTile) int {
-	return (abs(tile2.x - tile1.x) + 1) * (abs(tile2.y - tile1.y) + 1)
+	return (abs(tile2.x-tile1.x) + 1) * (abs(tile2.y-tile1.y) + 1)
 }
 
 func findMaxArea(tiles []RedTile) int {
 	maxArea := 0
 	for i, tile1 := range tiles {
-		for j := i+1; j < len(tiles); j++ {
+		for j := i + 1; j < len(tiles); j++ {
 			tile2 := tiles[j]
 			tileArea := area(tile1, tile2)
 			if tileArea > maxArea {
@@ -39,7 +52,29 @@ func findMaxArea(tiles []RedTile) int {
 	return maxArea
 }
 
-func makeGrid(tiles []RedTile) 
+func makeGrid(tiles []RedTile) DirectionGrid {
+	minX := tiles[0].x
+	maxX := tiles[0].x
+	minY := tiles[0].y
+	maxY := tiles[0].y
+	for _, tile := range tiles {
+		minX = min(minX, tile.x)
+		minY = min(minY, tile.y)
+		maxX = max(maxX, tile.x)
+		maxY = max(maxY, tile.y)
+	}
+	dimX := maxX - minX + 1
+	dimY := maxY - minY + 1
+	grid := make([][]Directions, dimY)
+	for i := range dimX {
+		grid[i] = make([]Directions, dimX)
+	}
+	return DirectionGrid{minX, minY, grid}
+}
+
+func (grid DirectionGrid) fill(tiles []RedTile) {
+
+}
 
 func day9() {
 	fileName := filepath.Join("inputs", "day9.in")
